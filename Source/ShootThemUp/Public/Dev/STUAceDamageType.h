a8945fc0534b2d2444951f250a3c678345c3096e
RFozzy688 <pokerfozzy@gmail.com>
2024-06-20 14:58:38 +0300

    4. Здоровье персонажа/ 5. Класс UDamageType

---
+++ Source/ShootThemUp/Public/Dev/STUAceDamageType.h
@@ -0,0 +1,17 @@
+// Shoot Them Up Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "STUAceDamageType.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API USTUAceDamageType : public UDamageType
+{
+    GENERATED_BODY()
+    
+};
