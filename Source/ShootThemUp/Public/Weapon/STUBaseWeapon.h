ef872605e9b37ba57938c7f3e7a9291ae64a2e6d
RFozzy688 <pokerfozzy@gmail.com>
2024-06-21 08:42:39 +0300

    5. Оружие/ 2. Класс ABaseWeapon

---
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -0,0 +1,31 @@
+// Shoot Them Up Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUBaseWeapon.generated.h"
+
+class USkeletalMeshComponent;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUBaseWeapon : public AActor
+{
+    GENERATED_BODY()
+    
+public:	
+    // Sets default values for this actor's properties
+    ASTUBaseWeapon();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+    USkeletalMeshComponent* WeaponMesh;
+
+public:	
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+};
