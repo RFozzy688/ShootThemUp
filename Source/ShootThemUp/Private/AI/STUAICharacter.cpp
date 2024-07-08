5f4c363d1b262c9caee8ed1e0f6572aa912b0aad
RFozzy688 <pokerfozzy@gmail.com>
2024-07-08 18:51:42 +0300

    9. Shoot Them Up AI/ 11. Остановка дерева поведения

--- Source/ShootThemUp/Private/AI/STUAICharacter.cpp
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -5,6 +5,10 @@
 #include "AI/STUAIController.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Components/STUAIWeaponComponent.h"
+#include "BrainComponent.h"
+#include "Components/WidgetComponent.h"
+//#include "UI/STUHealthBarWidget.h"
+#include "Components/STUHealthComponent.h"
 
 ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit) 
     : Super(ObjInit.SetDefaultSubobjectClass<USTUAIWeaponComponent>("WeaponComponent"))
@@ -17,5 +21,16 @@ ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit)
     {
         GetCharacterMovement()->bUseControllerDesiredRotation = true;
         GetCharacterMovement()->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
+    }
+}
+
+void ASTUAICharacter::OnDeath() 
+{
+    Super::OnDeath();
+
+    const auto STUController = Cast<AAIController>(Controller);
+    if (STUController && STUController->BrainComponent)
+    {
+        STUController->BrainComponent->Cleanup();
     }
 }
--- Source/ShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -49,6 +49,7 @@ protected:
 
     // Called when the game starts or when spawned
     virtual void BeginPlay() override;
+    virtual void OnDeath();
 
 public:	
     // Called every frame
@@ -73,7 +74,6 @@ private:
     void OnStartRunning();
     void OnStopRunning();
 
-    void OnDeath();
     void OnHealthChanged(float Health, float HealthDelta);
 
     UFUNCTION()
