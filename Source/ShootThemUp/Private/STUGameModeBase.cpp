0cc13c662a3e35cd0b43b9201bb53161aeeb5c3b
RFozzy688 <pokerfozzy@gmail.com>
2024-07-09 21:54:48 +0300

    10. Shoot Them Up Game Mode/ 3. Спаун AI персонажей

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -5,10 +5,47 @@
 #include "Player/STUBaseCharacter.h"
 #include "Player/STUPlayerController.h"
 #include "UI/STUGameHUD.h"
+#include "AIController.h"
 
 ASTUGameModeBase::ASTUGameModeBase()
 {
     DefaultPawnClass = ASTUBaseCharacter::StaticClass();
     PlayerControllerClass = ASTUPlayerController::StaticClass();
     HUDClass = ASTUGameHUD::StaticClass();
+}
+
+void ASTUGameModeBase::StartPlay() 
+{
+    Super::StartPlay();
+
+    SpawnBots();
+    //CreateTeamsInfo();
+
+    //CurrentRound = 1;
+    //StartRound();
+
+    //SetMatchState(ESTUMatchState::InProgress);
+}
+
+UClass* ASTUGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+    if (InController && InController->IsA<AAIController>())
+    {
+        return AIPawnClass;
+    }
+    return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void ASTUGameModeBase::SpawnBots() 
+{
+    if (!GetWorld()) return;
+
+    for (int32 i = 0; i < GameData.PlayersNum - 1; ++i)
+    {
+        FActorSpawnParameters SpawnInfo;
+        SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+        const auto STUAIController = GetWorld()->SpawnActor<AAIController>(AIControllerClass, SpawnInfo);
+        RestartPlayer(STUAIController);
+    }
 }
--- Source/ShootThemUp/Public/STUCoreTypes.h
+++ Source/ShootThemUp/Public/STUCoreTypes.h
@@ -87,3 +87,28 @@ struct FImpactData
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Sound")
     USoundCue* Sound;
 };
+
+// game
+USTRUCT(BlueprintType)
+struct FGameData
+{
+    GENERATED_USTRUCT_BODY()
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Game", meta = (ClampMin = "1", ClampMax = "100"))
+    int32 PlayersNum = 2;
+
+    //UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Game", meta = (ClampMin = "1", ClampMax = "10"))
+    //int32 RoundsNum = 4;
+
+    //UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Game", meta = (ClampMin = "3", ClampMax = "300"))
+    //int32 RoundTime = 10;  // in seconds
+
+    //UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    //FLinearColor DefaultTeamColor = FLinearColor::Red;
+
+    //UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    //TArray<FLinearColor> TeamColors;
+
+    //UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Game", meta = (ClampMin = "3", ClampMax = "20"))
+    //int32 RespawnTime = 5;  // in seconds
+};
