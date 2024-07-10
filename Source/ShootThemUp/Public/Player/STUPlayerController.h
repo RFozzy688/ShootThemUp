d0a3d5d6b47418a67018e847d03fb613f470ac2d
RFozzy688 <pokerfozzy@gmail.com>
2024-07-10 15:38:03 +0300

    10. Shoot Them Up Game Mode/ 13. Делегат OnNewPawn

--- Source/ShootThemUp/Public/Player/STUPlayerController.h
+++ Source/ShootThemUp/Public/Player/STUPlayerController.h
@@ -21,4 +21,6 @@ public:
 protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     USTURespawnComponent* RespawnComponent;
+
+    virtual void OnPossess(APawn* InPawn) override;
 };
