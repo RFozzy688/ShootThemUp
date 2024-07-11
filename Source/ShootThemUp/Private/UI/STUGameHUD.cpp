d6763a28397d72da36beb8107452f8f27683e3d8
RFozzy688 <pokerfozzy@gmail.com>
2024-07-11 09:48:18 +0300

    11. Shoot Them Up Интерфейсы игры/ 3. Пауза в игре. meta = (BindWidget)

--- Source/ShootThemUp/Private/UI/STUGameHUD.cpp
+++ Source/ShootThemUp/Private/UI/STUGameHUD.cpp
@@ -18,10 +18,17 @@ void ASTUGameHUD::DrawHUD()
 void ASTUGameHUD::BeginPlay() 
 {
     Super::BeginPlay();
-    auto PlayerHUDWidget = CreateWidget<UUserWidget>(GetWorld(), PlayerHUDWidgetClass);
-    if (PlayerHUDWidget)
+
+    GameWidgets.Add(ESTUMatchState::InProgress, CreateWidget<UUserWidget>(GetWorld(), PlayerHUDWidgetClass));
+    GameWidgets.Add(ESTUMatchState::Pause, CreateWidget<UUserWidget>(GetWorld(), PauseWidgetClass));
+
+    for (auto GameWidgetPair : GameWidgets)
     {
-        PlayerHUDWidget->AddToViewport();
+        const auto GameWidget = GameWidgetPair.Value;
+        if (!GameWidget) continue;
+
+        GameWidget->AddToViewport();
+        GameWidget->SetVisibility(ESlateVisibility::Hidden);
     }
 
     if (GetWorld())
@@ -50,21 +57,21 @@ void ASTUGameHUD::DrawCrossHair()
 
 void ASTUGameHUD::OnMatchStateChanged(ESTUMatchState State)
 {
-    //if (CurrentWidget)
-    //{
-    //    CurrentWidget->SetVisibility(ESlateVisibility::Hidden);
-    //}
+    if (CurrentWidget)
+    {
+        CurrentWidget->SetVisibility(ESlateVisibility::Hidden);
+    }
 
-    //if (GameWidgets.Contains(State))
-    //{
-    //    CurrentWidget = GameWidgets[State];
-    //}
+    if (GameWidgets.Contains(State))
+    {
+        CurrentWidget = GameWidgets[State];
+    }
 
-    //if (CurrentWidget)
-    //{
-    //    CurrentWidget->SetVisibility(ESlateVisibility::Visible);
-    //    CurrentWidget->Show();
-    //}
+    if (CurrentWidget)
+    {
+        CurrentWidget->SetVisibility(ESlateVisibility::Visible);
+        //CurrentWidget->Show();
+    }
 
     UE_LOG(LogSTUGameHUD, Display, TEXT("Match state changed: %s"), *UEnum::GetValueAsString(State));
 }
--- Source/ShootThemUp/Public/UI/STUGameHUD.h
+++ Source/ShootThemUp/Public/UI/STUGameHUD.h
@@ -7,9 +7,8 @@
 #include "STUCoreTypes.h"
 #include "STUGameHUD.generated.h"
 
-/**
- * 
- */
+class USTUBaseWidget;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTUGameHUD : public AHUD
 {
@@ -22,9 +21,18 @@ protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
     TSubclassOf<UUserWidget> PlayerHUDWidgetClass;
 
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
+    TSubclassOf<UUserWidget> PauseWidgetClass;
+
     virtual void BeginPlay() override;
 
 private:
+    UPROPERTY()
+    TMap<ESTUMatchState, UUserWidget*> GameWidgets;
+
+    UPROPERTY()
+    UUserWidget* CurrentWidget = nullptr;
+
     void DrawCrossHair();
     void OnMatchStateChanged(ESTUMatchState State);
 };
