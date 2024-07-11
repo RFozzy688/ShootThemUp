d1f49aa6e83ac5e6220b56c2893fc15069c7e1b3
RFozzy688 <pokerfozzy@gmail.com>
2024-07-11 07:50:03 +0300

    10. Shoot Them Up Game Mode/ 15. Исправление артефакта камеры

--- Source/ShootThemUp/Public/Player/STUPlayerCharacter.h
+++ Source/ShootThemUp/Public/Player/STUPlayerCharacter.h
@@ -8,6 +8,7 @@
 
 class UCameraComponent;
 class USpringArmComponent;
+class USphereComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUPlayerCharacter : public ASTUBaseCharacter
@@ -25,7 +26,11 @@ protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Component")
     USpringArmComponent* SpringArmComponent;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    USphereComponent* CameraCollisionComponent;
+
     virtual void OnDeath() override;
+    virtual void BeginPlay() override;
 
 public:
     // Called to bind functionality to input
@@ -42,4 +47,15 @@ private:
 
     void OnStartRunning();
     void OnStopRunning();
+
+    UFUNCTION()
+    void OnCameraCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, 
+        AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+        bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnCameraCollisionEndOverlap(UPrimitiveComponent* OverlappedComponent, 
+        AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+    void CheckCameraOverlap();
 };
